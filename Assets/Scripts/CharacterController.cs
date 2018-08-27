bd5e455e7316efe8a9a7de9735e4f8bf667765e5
invincible0918 <invincible0918@126.com>
2018-08-27 22:37:51 +0800

    add new scene

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    public Text text;
+
+    private PhysxAnimation[] _physxAnimations;
+
+    private bool _active = true;
+
+    public void OnClick()
+    {
+        _active = !_active;
+
+        if (_active)
+            text.text = "Physx Animation On";
+        else
+            text.text = "Physx Animation Off";
+
+        SetActivePhysxAnimation();
+    }
+
+    void Start ()
+    {
+        _physxAnimations = GetComponentsInChildren<PhysxAnimation>();
+    }
+	
+	void SetActivePhysxAnimation()
+    {
+        foreach(PhysxAnimation pa in _physxAnimations)
+        {
+            if (_active)
+                pa.UpdateMode = PhysxAnimation.AnimationUpdateMode.Update;
+            else
+                pa.UpdateMode = PhysxAnimation.AnimationUpdateMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCharacter : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    private float _lastX;
+    private Vector3 _lastRot;
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+            _lastX = Input.mousePosition.x;
+
+        if (Input.GetMouseButton(0))
+        {
+            float rotate = _lastX - Input.mousePosition.x;
+            transform.localEulerAngles = _lastRot + new Vector3(0, rotate * speed, 0);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+            _lastRot = transform.localEulerAngles;
+    }
+}
+
+
